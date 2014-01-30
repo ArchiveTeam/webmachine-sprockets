dec7a09338b35ec10cb885ebf67d0bc81f593e1b
Asmod4n <Asmod4n@users.noreply.github.com>
2014-01-30 12:38:07 +0100

    Namespace for Sprockets

--- lib/webmachine/sprockets.rb
+++ lib/webmachine/sprockets.rb
@@ -1,13 +1,12 @@
 require 'webmachine'
 require 'sprockets'
 
-require 'webmachine/sprockets/resource'
-require 'webmachine/sprockets/version'
-
 module Webmachine
   module Sprockets
-    def self.resource_for(sprockets)
-      Resource.dup.tap {|r| r.sprockets = sprockets }
-    end
+
   end
 end
+
+
+require 'webmachine/sprockets/resource'
+require 'webmachine/sprockets/version'
